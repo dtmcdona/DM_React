9a5f9dc1682bb03daff935929be25b5641dc3161
dtmcdona <davidmcdwork@gmail.com>
2022-05-10 19:15:03 -0700

    Added delete function to dynamic action list

--- app/src/components.js
+++ app/src/components.js
@@ -5,15 +5,17 @@ const Components = {
   action: Action,
 };
 
-export default (block) => {
+const Component = ({ block, delete_func }) => {
   if (typeof Components[block.component] !== "undefined") {
     return React.createElement(Components[block.component], {
-      key: block._uid,
+      key: block.id,
       block: block,
+      delete_func: delete_func,
     });
   }
   return React.createElement(
     () => <div>The component {block.component} has not been created yet.</div>,
-    { key: block._uid }
+    { key: block.id }
   );
 };
+export default Component;
--- app/src/components/Action.js
+++ app/src/components/Action.js
@@ -5,5 +5,10 @@ export default (props) => (
     <th>{props.block.id}</th>
     <th>{props.block.name}</th>
     <th>{props.block.code}</th>
+    <th>
+      <button name={props.block.id} onClick={props.delete_func}>
+        Delete
+      </button>
+    </th>
   </tr>
 );
